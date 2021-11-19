0a0599bf71f1ca7109cb9ae74b8e04804c17a3ca
domdelorenzo <delorenzo.breed@gmail.com>
2021-11-19 11:00:59 -0600

    Created ghost sprite

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
 /* define constants here */
 //constant to identify player sprite with CSS class
 const player = document.getElementById(`sprite`);
+const enemy = document.getElementById(`ghost`);
 
-const dotArray = [`1_2`, `1_3`, `1_4`, `2_2`, `2_4`, `2_5`, `3_5`];
+const dotArray = [
+  `1_2`,
+  `1_3`,
+  `1_4`,
+  `2_2`,
+  `2_4`,
+  `2_5`,
+  `2_6`,
+  `2_7`,
+  `2_8`,
+  `2_9`,
+  `3_9`,
+  `4_9`,
+  `5_9`,
+  `6_9`
+];
 let score = 0;
 /* define functions here */
 //set sprite initial position
@@ -11,6 +27,12 @@ const initialPosition = () => {
   player.style.gridRowStart = 2;
 };
 initialPosition();
+
+const enemyPosition = () => {
+  enemy.style.gridColumnStart = 9;
+  enemy.style.gridRowStart = 2;
+};
+enemyPosition();
 
 // retrieve sprite position at any time
 const getPosition = () => {
@@ -41,17 +63,8 @@ const gridMaker = (x, y) => {
           `beforeend`,
           `<div class="wall" id ="${i}_${j}"></div>`
         );
-      // let newDiv = document.createElement(`div`);
-      // newDiv.setAttribute('id', `${i}_${j}`);
-      // // newDiv.className = 'dot';
-      // // newDiv.style.gridRowStart = y + 1;
-      // // newDiv.style.gridColumnStart = x + 1;
-      // console.log('new div created');
-      // document.querySelector(`.map`).appendChild.newDiv;
-      // // document.querySelector(`.map`).insertAdjacentHTML(`afterbegin`,`<div class="dot"></div>`)
     }
   }
-  // document.querySelector(`.map`).insertAdjacentHTML(`afterbegin`,`<div class="dot"></div>`)
 };
 
 gridMaker(11, 11);
@@ -146,3 +159,13 @@ document.addEventListener('keydown', logKey);
 // const board = document.querySelector(`.map`);
 // newDiv.setAttribute('id', '1_3');
 // board.appendChild.newDiv;
+// let newDiv = document.createElement(`div`);
+// newDiv.setAttribute('id', `${i}_${j}`);
+// // newDiv.className = 'dot';
+// // newDiv.style.gridRowStart = y + 1;
+// // newDiv.style.gridColumnStart = x + 1;
+// console.log('new div created');
+// document.querySelector(`.map`).appendChild.newDiv;
+// // document.querySelector(`.map`).insertAdjacentHTML(`afterbegin`,`<div class="dot"></div>`)
+
+// document.querySelector(`.map`).insertAdjacentHTML(`afterbegin`,`<div class="dot"></div>`)
